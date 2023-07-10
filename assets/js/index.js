11160d807031da5835f9ad975339dd81ce9ee62f
CafNotas <138479553+CafNotas@users.noreply.github.com>
2023-07-10 07:35:38 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,14 @@
-let btn = document.querySelector('.fa-eye');
+let btn = document.querySelector('.fa-eye')
 
 btn.addEventListener('click', ()=>{
-  let inputSenha = document.querySelector('#senha');
+  let inputSenha = document.querySelector('#senha')
   
   if(inputSenha.getAttribute('type') == 'password'){
     inputSenha.setAttribute('type', 'text')
   } else {
     inputSenha.setAttribute('type', 'password')
-  };
-});
+  }
+})
 
 function entrar(){
   let usuario = document.querySelector('#usuario');
