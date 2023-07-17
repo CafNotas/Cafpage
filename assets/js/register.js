56606abf641a85a13cc08124566265fd3de6641d
CafNotas <138479553+CafNotas@users.noreply.github.com>
2023-07-17 07:43:22 -0300

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -110,7 +110,7 @@ function cadastrar(){
   }
 }
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('mouseout', ()=>{
   let inputSenha = document.querySelector('#senha')
   
   if(inputSenha.getAttribute('type') == 'password'){
@@ -120,7 +120,27 @@ btn.addEventListener('click', ()=>{
   }
 })
 
-btnConfirm.addEventListener('click', ()=>{
+btn.addEventListener('mouseover', () => {
+  let inputSenha = document.querySelector('#senha')
+
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
+
+btnConfirm.addEventListener('mouseout', ()=>{
+  let inputConfirmSenha = document.querySelector('#confirmSenha')
+  
+  if(inputConfirmSenha.getAttribute('type') == 'password'){
+    inputConfirmSenha.setAttribute('type', 'text')
+  } else {
+    inputConfirmSenha.setAttribute('type', 'password')
+  }
+})
+
+btnConfirm.addEventListener('mouseover', ()=>{
   let inputConfirmSenha = document.querySelector('#confirmSenha')
   
   if(inputConfirmSenha.getAttribute('type') == 'password'){
