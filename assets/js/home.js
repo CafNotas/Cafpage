fee7cc4bc5640c8b55980f766b5c797f219267ab
CafNotas <138479553+CafNotas@users.noreply.github.com>
2023-07-17 12:04:38 -0300

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -107,7 +107,7 @@ links.onclick = function clear() {
                 <a class="link-bt" href="http://scaweb.saude.gov.br/scaweb/" target="_blank">
                 <div>
                     <img class="logo-op"
-                        src="../img/MS.png"
+                        src="../img/HORUS.png"
                         alt="logo prefeitura">
                 </div>
             </a>
